179c2698b014267cc24bf6e5f30309a9c5a26029
Firuz43 <firuz1199@mail.ru>
2022-04-11 14:41:00 +0400

    url changes and database

--- main/api/models.py
+++ main/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    surname = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- main/main/settings.py
+++ main/main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myapp',
+        'USER': 'postgres',
+        'PASSWORD': 'firuz1999',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
